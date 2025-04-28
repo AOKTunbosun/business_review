c103ea89a26b83fbe908eceb48cb383df217bd42
AOKTunbosun <aokolatunbosun@gmail.com>
2025-04-28 21:41:24 +0100

    Login and home search bar -- I integrate the login page and all its functionalities and i also added the home search bar functionalities

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .forms import ReviewForm
 from .models import Business, Category
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db.models import Q
 from django.http import HttpResponse
 
 # businesses = [
@@ -13,15 +17,40 @@ from django.http import HttpResponse
 def home(request):
     businesses = Business.objects.all()
     categories = Category.objects.all()
-    print(businesses)
     context = {'businesses': businesses, 'categories': categories}
     return render(request, 'base/home.html', context)
 
 def businesses(request):
-    businesses = Business.objects.all()
+    b = request.GET.get('b') if request.GET.get('b') != None else ''
+    l = request.GET.get('l') if request.GET.get('l') != None else ''
+
+    if b != '' and l != '':
+        businesses = Business.objects.filter(
+            Q(name__icontains=b) |
+            Q(category__name__icontains=b) |
+            Q(address__icontains=l) |
+            Q(city__icontains=l)
+        )
+
+    elif b != '' and l == '':
+        businesses = Business.objects.filter(
+            Q(name__icontains=b) |
+            Q(category__name__icontains=b)
+        )
+
+    elif b == '' and l != '':
+        businesses = Business.objects.filter(
+            Q(address__icontains=l) |
+            Q(city__icontains=l)
+        )
+
+    else:
+        businesses = Business.objects.filter()
+
+
     context = {'businesses': businesses}
     return render(request, 'base/businesses.html', context )
-    # return HttpResponse('Businesses')
+
 
 
 def business_page(request, pk):
@@ -31,7 +60,33 @@ def business_page(request, pk):
     return render(request, 'base/business_page.html', context)
 
 def login_page(request):
-    return render(request, 'base/login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+
+        try:
+            user = User.objects.get(username=username)
+
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Password is incorrect')
+
+
+    context = {}
+    return render(request, 'base/login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
 
 def signup_page(request):
     return render(request, 'base/signup.html')
